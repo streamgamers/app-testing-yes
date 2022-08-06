1b6e563e912390dcf5e5cd200e93d0294aff0519
streamgamers <110721371+streamgamers@users.noreply.github.com>
2022-08-06 18:01:28 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,8 @@
+from flask import request
 
-from flask import Flask, jsonify, request
-from pytube import YouTube
-import jsonpickle
-import os
-import sys
-
-app = Flask(__name__)
-
-@app.route('/youtube', methods=['POST'])
-def hello(start_response):
-    start_response('200 OK', [('Content-Type', 'application/json')])
-    url = request.form['d_url']
-    yt_video = YouTube(url)
-    videos = yt_video.streams
-    res_list = list(enumerate(videos))
-    j = jsonpickle.encode(res_list)
-
-    return j
-
-    
-    
-    
-if __name__ == "__main__":
-    app.run(debug =True)
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        return "Post Method"
+    else:
+        return "Get Method"
