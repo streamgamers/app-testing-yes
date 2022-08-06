edf75c1cd5a5c91514b4f4fba3375389b10e0368
streamgamers <110721371+streamgamers@users.noreply.github.com>
2022-08-06 18:02:41 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import request
-
+app = Flask(__name__)
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         return "Post Method"
     else:
         return "Get Method"
+    
+if __name__ == "__main__":
+    app.run(debug =True)
