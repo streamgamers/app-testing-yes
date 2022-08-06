5b44948056e13fec1a93d07805c71ed0671694f6
streamgamers <110721371+streamgamers@users.noreply.github.com>
2022-08-06 18:41:51 +0500

    Update app.py

--- app.py
+++ app.py
@@ -7,11 +7,11 @@ app = Flask(__name__)
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-           return request.form['d_url']
-#          yt_video = YouTube(request.form["d_url"])
-#          videos = yt_video.streams.filter(only_audio=True).all()
-#          res_list = list(videos)
-#          return jsonpickle.encode(res_list)
+#            return request.form['d_url']
+         yt_video = YouTube(request.form["d_url"])
+         videos = yt_video.streams
+         res_list = list(videos)
+         return jsonpickle.encode(res_list)
 #            return yt_video
 
         
