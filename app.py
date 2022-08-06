468d8be684236a9b3448722b61768377f4eb956a
streamgamers <110721371+streamgamers@users.noreply.github.com>
2022-08-06 17:35:15 +0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ import jsonpickle
 app = Flask(__name__)
 
 @app.route('/youtube', methods=['POST'])
-def app():
+def app(environ, start_response):
+    start_response('200 OK', [('Content-Type', 'application/json')])
     url = request.form['d_url']
     yt_video = YouTube(url)
     videos = yt_video.streams
