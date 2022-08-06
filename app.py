b473a2499496f1d004266db5cd97f77a599eca68
streamgamers <110721371+streamgamers@users.noreply.github.com>
2022-08-06 17:18:02 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
-from fastapi import FastAPI
 
-app = FastAPI()
+from flask import Flask, jsonify, request
+from pytube import YouTube
+import jsonpickle
+app = Flask(__name__)
 
-@app.get("/my-first-api")
-def hello(name = None):
+@app.route('/youtube', methods=['POST'])
+def hello_world():
+    url = request.form['d_url']
+    yt_video = YouTube(url)
+    videos = yt_video.streams
+    res_list = list(enumerate(videos))
+    j = jsonpickle.encode(res_list)
 
-    if name is None:
-        text = 'Hello!'
+    return j
 
-    else:
-        text = 'Hello ' + name + '!'
-
-    return text
+    
+    
+    
+if __name__ == "__main__":
+    app.run(debug =True)
