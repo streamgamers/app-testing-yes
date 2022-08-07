e4187a21ac249a8e7621be94973bc7b2255652e7
streamgamers <110721371+streamgamers@users.noreply.github.com>
2022-08-07 16:15:48 +0500

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,10 @@ from flask import Flask
 
 # import jsonpickle
 app = Flask(__name__)
-@app.route('/login', methods=['GET', 'POST'])
+# @app.route('/login', methods=['GET', 'POST'])
+@app.route('/login')
 def login():
-    if request.method == 'POST':
+#     if request.method == 'POST':
 #            return request.form['d_url']
          yt_video = YouTube('https://www.youtube.com/watch?v=p07ZZZVL72E')
          return yr_video.title
@@ -17,8 +18,8 @@ def login():
 #            return yt_video
 
         
-    else:
-        return request.args.get('d_url')
+#     else:
+#         return request.args.get('d_url')
     
 if __name__ == "__main__":
     app.run(debug =False)
