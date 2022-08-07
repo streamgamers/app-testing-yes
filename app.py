094d30913f0f72ac2c94766c1f2c2efa8ca7c933
streamgamers <110721371+streamgamers@users.noreply.github.com>
2022-08-07 14:59:10 +0500

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def login():
     if request.method == 'POST':
 #            return request.form['d_url']
-         yt_video = YouTube(request.form["d_url"])
+         yt_video = YouTube(request.form['d_url'])
          return yr_video.title
 #          videos = yt_video.streams
 #          res_list = list(videos)
